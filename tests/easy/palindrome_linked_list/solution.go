6440fa1171b2a187ae01dda4cf5992b6e5e9426a
z411392 <z411392@gmail.com>
2025-01-27 22:06:04 +0800

    Mon Jan 27 22:06:04 CST 2025

---
+++ tests/easy/palindrome_linked_list/solution.go
@@ -0,0 +1,14 @@
+package palindrome_linked_list
+
+//lint:file-ignore ST1001 _
+
+import (
+	. "github.com/z411392/leetcode/pkg/data_structure/linked_list"
+)
+
+/*
+Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+*/
+func isPalindrome(head *ListNode) bool {
+	return false
+}
---
+++ tests/easy/palindrome_linked_list/solution_test.go
@@ -0,0 +1,25 @@
+package palindrome_linked_list
+
+import (
+	"testing"
+
+	. "github.com/z411392/leetcode/pkg/data_structure/linked_list"
+)
+
+func Test_isPalindrome_1(t *testing.T) {
+	// t.SkipNow()
+	got := isPalindrome(NewLinkedListFromSlice([]int{1, 2, 2, 1}))
+	expected := true
+	if got != expected {
+		t.FailNow()
+	}
+}
+
+func Test_isPalindrome_2(t *testing.T) {
+	// t.SkipNow()
+	got := isPalindrome(NewLinkedListFromSlice([]int{1, 2}))
+	expected := false
+	if got != expected {
+		t.FailNow()
+	}
+}
