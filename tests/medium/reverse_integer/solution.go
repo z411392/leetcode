41cab8b4031b720bf60b564db30c62ef48190113
z411392 <z411392@gmail.com>
2025-02-01 15:52:00 +0800

    Sat Feb  1 15:52:00 CST 2025

--- tests/medium/reverse_integer/solution.go
+++ tests/medium/reverse_integer/solution.go
@@ -1,33 +1,22 @@
 package reverse_integer
 
-import (
-	"strconv"
-	"strings"
-)
-
 /*
 Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 
 Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
 */
 func reverse(x int) int {
-	if x == 0 {
-		return 0
+	result := 0
+	for x != 0 {
+		digit := x % 10
+		// fmt.Printf("digit=%v\n", digit)
+		next := result*10 + digit // 合併起來一次檢查
+		// fmt.Printf("result=%v next=%v\n", result, next)
+		if int32(next)/10 != int32(result) { // 按整數除法運算的特性會忽略餘數 而且此時 result 都還是加上 digit 之前的值
+			return 0
+		}
+		x = x / 10
+		result = next
 	}
-	str := strconv.Itoa(x)
-	stringBuilder := &strings.Builder{}
-	if strings.HasPrefix(str, "-") {
-		stringBuilder.WriteRune('-')
-		str = strings.ReplaceAll(str, "-", "")
-	}
-	// fmt.Printf("%v %v\n", x, str)
-	for i := len(str) - 1; i > -1; i-- {
-		stringBuilder.WriteByte(str[i])
-	}
-	stringified := stringBuilder.String()
-	result, err := strconv.ParseInt(stringified, 10, 32)
-	if err != nil {
-		return 0
-	}
-	return int(result)
+	return result
 }
