0bb51575a6671d7d7a246d4eb1f18d9375673b30
z411392 <z411392@gmail.com>
2025-02-05 18:58:05 +0800

    Wed Feb  5 18:58:04 CST 2025

--- tests/medium/set_matrix_zeroes/solution.go
+++ tests/medium/set_matrix_zeroes/solution.go
@@ -6,35 +6,24 @@ Given an m x n integer matrix matrix, if an element is 0, set its entire row and
 func setZeroes(matrix [][]int) {
 	rows := map[int]bool{}
 	columns := map[int]bool{}
-	reset := map[[2]int]bool{}
-	setRow := func(row int) {
-		if rows[row] {
-			return
-		}
-		for column := range matrix[row] {
-			reset[[2]int{row, column}] = true
-		}
-		rows[row] = true
-	}
-	setColumn := func(column int) {
-		if columns[column] {
-			return
-		}
-		for row := 0; row < len(matrix); row += 1 {
-			reset[[2]int{row, column}] = true
-		}
-		columns[column] = true
-	}
 	for row, cells := range matrix {
 		for column, cell := range cells {
 			if cell != 0 {
 				continue
 			}
-			setRow(row)
-			setColumn(column)
+			rows[row] = true
+			columns[column] = true
 		}
 	}
-	for position := range reset {
-		matrix[position[0]][position[1]] = 0
+	// 假設 rows=4 columns=3 組在一起跑是 4x3=12，但分開跑是 4+3=7
+	for row := range rows {
+		for column := range matrix[row] {
+			matrix[row][column] = 0
+		}
+	}
+	for column := range columns {
+		for row := range matrix {
+			matrix[row][column] = 0
+		}
 	}
 }
