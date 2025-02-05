64a8432b17cff2ad302eaa5405afb22ab7a66aa2
z411392 <z411392@gmail.com>
2025-02-05 18:46:53 +0800

    Wed Feb  5 18:46:53 CST 2025

--- tests/medium/set_matrix_zeroes/solution.go
+++ tests/medium/set_matrix_zeroes/solution.go
@@ -6,19 +6,35 @@ Given an m x n integer matrix matrix, if an element is 0, set its entire row and
 func setZeroes(matrix [][]int) {
 	rows := map[int]bool{}
 	columns := map[int]bool{}
-	for row, cells := range matrix {
-		for column, cell := range cells {
-			if cell == 0 {
-				rows[row] = true
-				columns[column] = true
-			}
+	reset := map[[2]int]bool{}
+	setRow := func(row int) {
+		if rows[row] {
+			return
 		}
+		for column := range matrix[row] {
+			reset[[2]int{row, column}] = true
+		}
+		rows[row] = true
+	}
+	setColumn := func(column int) {
+		if columns[column] {
+			return
+		}
+		for row := 0; row < len(matrix); row += 1 {
+			reset[[2]int{row, column}] = true
+		}
+		columns[column] = true
 	}
 	for row, cells := range matrix {
-		for column := range cells {
-			if rows[row] || columns[column] {
-				matrix[row][column] = 0
+		for column, cell := range cells {
+			if cell != 0 {
+				continue
 			}
+			setRow(row)
+			setColumn(column)
 		}
+	}
+	for position := range reset {
+		matrix[position[0]][position[1]] = 0
 	}
 }
