0f88c0a62c8c58ec7cf3699b721370bb06237b00
z411392 <z411392@gmail.com>
2025-02-05 11:51:38 +0800

    Wed Feb  5 11:51:38 CST 2025

--- tests/medium/maximum_subarray/solution.go
+++ tests/medium/maximum_subarray/solution.go
@@ -5,18 +5,16 @@ Given an integer array nums, find the subarray with the largest sum, and return 
 */
 func maxSubArray(nums []int) int {
 	// https://www.shubo.io/maximum-subarray-problem-kadane-algorithm/
-	result := 0
-	ending := 0
-	maximum := -10000
+	// https://leetcode.com/problems/maximum-subarray/description/
+	acc := nums[0]
+	sum := 0
 	for _, num := range nums {
-		ending = max(0, ending+num)
-		result = max(result, ending)
-		if num > maximum {
-			maximum = num
+		if sum < 0 {
+			sum = 0
 		}
+		// sum <=0 && num < 0 時也不能略過，因為如果全都是負數還是得找出一個值比較小的負數
+		sum += num
+		acc = max(acc, sum)
 	}
-	if result <= 0 {
-		return maximum
-	}
-	return result
+	return acc
 }
--- tests/medium/maximum_subarray/solution_test.go
+++ tests/medium/maximum_subarray/solution_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func Test_maxSubArray_1(t *testing.T) {
-	// t.SkipNow()
+	t.SkipNow()
 	got := maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4})
 	expected := 6
 	if got != expected {
@@ -14,7 +14,7 @@ func Test_maxSubArray_1(t *testing.T) {
 }
 
 func Test_maxSubArray_2(t *testing.T) {
-	// t.SkipNow()
+	t.SkipNow()
 	got := maxSubArray([]int{1})
 	expected := 1
 	if got != expected {
@@ -23,7 +23,7 @@ func Test_maxSubArray_2(t *testing.T) {
 }
 
 func Test_maxSubArray_3(t *testing.T) {
-	// t.SkipNow()
+	t.SkipNow()
 	got := maxSubArray([]int{5, 4, -1, 7, 8})
 	expected := 23
 	if got != expected {
