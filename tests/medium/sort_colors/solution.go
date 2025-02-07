b898e6401a0c7e757b531e83989066859f89156b
z411392 <z411392@gmail.com>
2025-02-07 14:43:42 +0800

    Fri Feb  7 14:43:42 CST 2025

--- tests/medium/sort_colors/solution.go
+++ tests/medium/sort_colors/solution.go
@@ -1,5 +1,12 @@
 package sort_colors
 
 func sortColors(nums []int) {
-
+	for i := 0; i < len(nums)-1; i += 1 {
+		for j := i + 1; j < len(nums); j += 1 {
+			if nums[i] <= nums[j] {
+				continue
+			}
+			nums[i], nums[j] = nums[j], nums[i]
+		}
+	}
 }
