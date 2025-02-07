42cfa018b3d7dddb6d77c256224463fb6bfdfad8
z411392 <z411392@gmail.com>
2025-02-07 15:53:56 +0800

    Fri Feb  7 15:53:56 CST 2025

--- tests/medium/subsets/solution.go
+++ tests/medium/subsets/solution.go
@@ -1,5 +1,16 @@
 package subsets
 
 func subsets(nums []int) [][]int {
-	return [][]int{}
+	subsets := [][]int{{}}
+	if len(nums) == 0 {
+		return subsets
+	}
+	for _, num := range nums {
+		for j := range subsets {
+			subset := append([]int{}, subsets[j]...)
+			// 保留原有的 subsets 直接再複製一份
+			subsets = append(subsets, append(subset, num))
+		}
+	}
+	return subsets
 }
--- tests/medium/subsets/solution_test.go
+++ tests/medium/subsets/solution_test.go
@@ -7,8 +7,7 @@ import (
 
 func Test_subsets_1(t *testing.T) {
 	// t.SkipNow()
-	got := []int{1, 2, 3}
-	subsets(got)
+	got := subsets([]int{1, 2, 3})
 	expected := [][]int{{}, {1}, {2}, {1, 2}, {3}, {1, 3}, {2, 3}, {1, 2, 3}}
 	if !reflect.DeepEqual(got, expected) {
 		t.Fatalf("expected %v, got %v\n", expected, got)
@@ -17,8 +16,7 @@ func Test_subsets_1(t *testing.T) {
 
 func Test_subsets_2(t *testing.T) {
 	// t.SkipNow()
-	got := []int{0}
-	subsets(got)
+	got := subsets([]int{0})
 	expected := [][]int{{}, {0}}
 	if !reflect.DeepEqual(got, expected) {
 		t.Fatalf("expected %v, got %v\n", expected, got)
