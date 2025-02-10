a138f34ba8dcba5c40c4c40c2aee7776c6062741
z411392 <z411392@gmail.com>
2025-02-10 13:25:23 +0800

    Mon Feb 10 13:25:23 CST 2025

--- tests/medium/populating_next_right_pointers_in_each_node/solution.go
+++ tests/medium/populating_next_right_pointers_in_each_node/solution.go
@@ -7,30 +7,31 @@ type Node struct {
 	Next  *Node
 }
 
-func connect(root *Node) *Node {
-	if root == nil {
-		return nil
-	}
-	queue := []*Node{root}
+func iterateOnLevel(node *Node, fun func(node *Node)) {
 	for {
-		nodes := queue
-		queue = []*Node{}
-		for i := 0; i < len(nodes); i += 1 {
-			node := nodes[i]
-			if i >= 1 {
-				lastNode := nodes[i-1]
-				lastNode.Next = node
-			}
-			if node.Left != nil {
-				queue = append(queue, node.Left)
-			}
-			if node.Right != nil {
-				queue = append(queue, node.Right)
-			}
-		}
-		if len(queue) == 0 {
+		if node == nil {
 			break
 		}
+		fun(node)
+		node = node.Next
+	}
+}
+
+func connect(root *Node) *Node {
+	node := root
+	for {
+		// 因為總是在這一層處理下一層的 next 關係。所以如果沒有下一層，就可以提早結束了。
+		if node == nil || node.Left == nil {
+			break
+		}
+		iterateOnLevel(node, func(node *Node) {
+			// 除了將左節點的 Next 設為右節點外，還要將右節點的 Next 設為 Next 的左節點。
+			node.Left.Next = node.Right
+			if node.Next != nil {
+				node.Right.Next = node.Next.Left
+			}
+		})
+		node = node.Left
 	}
 	return root
 }
