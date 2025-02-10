d5935bc700c2de8b0dcae3f77661101bc7240466
z411392 <z411392@gmail.com>
2025-02-10 15:01:04 +0800

    Mon Feb 10 15:01:04 CST 2025

--- tests/medium/best_time_to_buy_and_sell_stock_ii/solution.go
+++ tests/medium/best_time_to_buy_and_sell_stock_ii/solution.go
@@ -8,5 +8,15 @@ On each day, you may decide to buy and/or sell the stock. You can only hold at m
 Find and return the maximum profit you can achieve.
 */
 func maxProfit(prices []int) int {
-	return 0
+	profit := 0
+	prev := prices[0]
+	// 如果存在有價差的組合不會因為早賣或晚賣而總額有所不同；也就是說，一但有利潤就直接賣出的結果會跟各種組合串遍之後得到的最佳結果是一樣的
+	for i := 1; i < len(prices); i++ {
+		if prices[i]-prev > 0 {
+			profit += prices[i] - prev
+		}
+		prev = prices[i]
+	}
+
+	return profit
 }
