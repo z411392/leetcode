8663eaf1811957b69e578721453b2591c1f146d1
z411392 <z411392@gmail.com>
2025-02-10 16:48:37 +0800

    Mon Feb 10 16:48:37 CST 2025

--- tests/medium/longest_consecutive_sequence/solution.go
+++ tests/medium/longest_consecutive_sequence/solution.go
@@ -1,56 +1,32 @@
 package longest_consecutive_sequence
 
-import (
-	"slices"
-)
-
 /*
 Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
 
 You must write an algorithm that runs in O(n) time.
 */
 func longestConsecutive(nums []int) int {
-	slices.SortFunc(nums, func(a int, b int) int {
-		if a < b {
-			return -1
-		}
-		if a == b {
-			return 0
-		}
-		return 1
-	})
-	var startsWith any = nil
-	var lastNum any = nil
-	currentLength := 0
-	maxLength := 0
+	seen := make(map[int]bool, len(nums))
 	for _, num := range nums {
-		_, ok := startsWith.(int)
-		// fmt.Printf("數字 %v\n", num)
-		if !ok {
-			// fmt.Printf("頭一個數字 %v\n", num)
-			startsWith = num
-			lastNum = num
-			currentLength = 1
-			maxLength = 1
-			continue
-		}
-		lastNum = lastNum.(int)
-		if lastNum == num {
-			continue
-		}
-		if lastNum != num-1 {
-			// fmt.Printf("當前數字 %v 與前一個數字 %v 不連續\n", num, lastNum)
-			startsWith = num
-			lastNum = num
-			currentLength = 1
-			continue
-		}
-		currentLength += 1
-		if currentLength > maxLength {
-			// fmt.Printf("連續次數 %v 大於最大值 %v\n", currentLength, maxLength)
-			maxLength = currentLength
-		}
-		lastNum = num
+		seen[num] = true
 	}
-	return maxLength
+	var longest int
+	for num := range seen {
+		_, exists := seen[num-1]
+		// num-1 存在表不為連續數列的起點
+		if exists {
+			continue // continue 不會使 n 成爲 n logn 或 n^2
+		}
+		// 計算連續次數
+		length := 1
+		for {
+			_, exists := seen[num+length]
+			if !exists {
+				break
+			}
+			length += 1
+		}
+		longest = max(longest, length)
+	}
+	return longest
 }
