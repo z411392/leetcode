d8950a94241a342200d038bdeb48fd0054ca4711
z411392 <z411392@gmail.com>
2025-02-11 17:19:42 +0800

    Tue Feb 11 17:19:42 CST 2025

--- tests/medium/lru_cache/solution.go
+++ tests/medium/lru_cache/solution.go
@@ -1,7 +1,5 @@
 package lru_cache
 
-import "slices"
-
 /*
 Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
 
@@ -12,44 +10,89 @@ int get(int key) Return the value of the key if the key exists, otherwise return
 void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
 The functions get and put must each run in O(1) average time complexity.
 */
+type Node struct {
+	Key   int
+	Value int
+	Prev  *Node
+	Next  *Node
+}
+
 type LRUCache struct {
-	Capacity int
-	Values   map[int]int
-	Keys     []int
+	capacity int
+	cache    map[int]*Node
+	head     *Node
+	tail     *Node
 }
 
 func Constructor(capacity int) LRUCache {
-	return LRUCache{
-		Capacity: capacity,
-		Values:   map[int]int{},
-		Keys:     []int{},
+	// 預分配 capacity 大小的 map
+	lru := LRUCache{
+		capacity: capacity,
+		cache:    make(map[int]*Node, capacity),
+		head:     &Node{},
+		tail:     &Node{},
 	}
-}
-
-func (this *LRUCache) touch(key int) {
-	index := slices.Index(this.Keys, key)
-	if index > -1 {
-		this.Keys = append(this.Keys[:index], this.Keys[index+1:]...)
-	}
-	this.Keys = append(this.Keys, key)
+	lru.head.Next = lru.tail
+	lru.tail.Prev = lru.head
+	return lru
 }
 
 func (this *LRUCache) Get(key int) int {
-	value, exists := this.Values[key]
-	if !exists {
-		return -1
+	if node, exists := this.cache[key]; exists {
+		// 直接在這裡內聯 moveToEnd 的邏輯
+		// 1. 從當前位置移除
+		node.Prev.Next = node.Next
+		node.Next.Prev = node.Prev
+
+		// 2. 移動到尾部
+		lastNode := this.tail.Prev
+		lastNode.Next = node
+		node.Prev = lastNode
+		node.Next = this.tail
+		this.tail.Prev = node
+
+		return node.Value
 	}
-	this.touch(key)
-	return value
+	return -1
 }
 
 func (this *LRUCache) Put(key int, value int) {
-	this.Values[key] = value
-	this.touch(key)
-	if len(this.Keys) > this.Capacity {
-		key := this.Keys[0]
-		delete(this.Values, key)
-		this.Keys = this.Keys[1:]
+	if node, exists := this.cache[key]; exists {
+		// 更新值
+		node.Value = value
 
+		// 直接內聯 moveToEnd 的邏輯
+		node.Prev.Next = node.Next
+		node.Next.Prev = node.Prev
+
+		lastNode := this.tail.Prev
+		lastNode.Next = node
+		node.Prev = lastNode
+		node.Next = this.tail
+		this.tail.Prev = node
+		return
+	}
+
+	// 創建新節點
+	newNode := &Node{
+		Key:   key,
+		Value: value,
+	}
+	this.cache[key] = newNode
+
+	// 直接加到尾部
+	lastNode := this.tail.Prev
+	lastNode.Next = newNode
+	newNode.Prev = lastNode
+	newNode.Next = this.tail
+	this.tail.Prev = newNode
+
+	// 檢查容量
+	if len(this.cache) > this.capacity {
+		// 移除最久未使用的節點
+		lru := this.head.Next
+		this.head.Next = lru.Next
+		lru.Next.Prev = this.head
+		delete(this.cache, lru.Key)
 	}
 }
