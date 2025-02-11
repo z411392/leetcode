44daf1ef501e16971f9840d45835641ac26f5415
z411392 <z411392@gmail.com>
2025-02-11 12:22:49 +0800

    Tue Feb 11 12:22:49 CST 2025

--- tests/medium/palindrome_partitioning/solution.go
+++ tests/medium/palindrome_partitioning/solution.go
@@ -1,35 +1,64 @@
 package palindrome_partitioning
 
-func getAllPartitions(s string) [][]string {
-	partitions := [][]string{}
-	if len(s) == 0 {
-		return partitions
-	}
-	if len(s) > 1 {
-		for i := 1; i <= len(s); i += 1 {
-			one := s[0:i]
-			subPartitions := getAllPartitions(s[i:])
-			for _, subPartition := range subPartitions {
-				partition := append([]string{one}, subPartition...)
-				partitions = append(partitions, partition)
-			}
-		}
-	}
-	partitions = append(partitions, []string{s})
-	return partitions
-}
+type IsPalindrome = func(s string) bool
 
-func isPalindrome(s string) bool {
-	if len(s) == 1 {
+func isPalindromeFunc() IsPalindrome {
+	cache := map[string]bool{}
+	return func(s string) bool {
+		if len(s) <= 1 {
+			return true
+		}
+		if len(s) == 2 {
+			return s[0] == s[1]
+		}
+		if isPalindrome, exists := cache[s]; exists {
+			return isPalindrome
+		}
+		i := 0
+		j := len(s) - 1
+		for {
+			if i >= j {
+				break
+			}
+			if s[i] != s[j] {
+				cache[s] = false
+				return false
+			}
+			i += 1
+			j -= 1
+		}
+		cache[s] = true
 		return true
 	}
-	if len(s) == 2 {
-		return s[0] == s[1]
+}
+
+type Backtrack = func(start int)
+type WhenFound = func(partition []string)
+
+func backtrackFunc(s string, isPalindrome IsPalindrome, whenFound WhenFound) Backtrack {
+	current := []string{}
+	var backtrack Backtrack
+	backtrack = func(start int) {
+		// 直到所有字元都用盡時，取用當前堆疊。
+		if start > len(s)-1 {
+			partition := make([]string, len(current))
+			copy(partition, current)
+			whenFound(partition)
+			return
+		}
+		// 往後找，若找到迴文，將它推入堆疊並繼續往下分解。
+		for end := start + 1; end <= len(s); end += 1 {
+			str := s[start:end]
+			if !isPalindrome(str) {
+				continue
+			}
+			current = append(current, str)
+			backtrack(end)
+			// 處理完時記得將迴文從堆疊取出
+			current = current[:len(current)-1]
+		}
 	}
-	if s[0] != s[len(s)-1] {
-		return false
-	}
-	return isPalindrome(s[1 : len(s)-1])
+	return backtrack
 }
 
 /*
@@ -44,19 +73,10 @@ func partition(s string) [][]string {
 	if len(s) <= 1 {
 		return partitions
 	}
-	for _, partition := range getAllPartitions(s) {
-		found := true
-		for _, str := range partition {
-			// fmt.Printf("%s\n", str)
-			if !isPalindrome(str) {
-				found = false
-				break
-			}
-		}
-		if !found {
-			continue
-		}
+
+	backtrack := backtrackFunc(s, isPalindromeFunc(), func(partition []string) {
 		partitions = append(partitions, partition)
-	}
+	})
+	backtrack(0)
 	return partitions
 }
