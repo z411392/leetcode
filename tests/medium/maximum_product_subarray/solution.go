da33d95bf44dda3dc06b81d42895028cf23a5f03
z411392 <z411392@gmail.com>
2025-02-12 14:56:38 +0800

    Wed Feb 12 14:56:38 CST 2025

--- tests/medium/maximum_product_subarray/solution.go
+++ tests/medium/maximum_product_subarray/solution.go
@@ -6,5 +6,20 @@ Given an integer array nums, find a subarray that has the largest product, and r
 The test cases are generated so that the answer will fit in a 32-bit integer.
 */
 func maxProduct(nums []int) int {
-	return 0
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	currentMax := nums[0]
+	currentMin := nums[0]
+	maxProduct := nums[0]
+	for _, num := range nums[1:] {
+		temp := currentMax
+		currentMax = max(num, max(temp*num, currentMin*num))
+		currentMin = min(num, min(temp*num, currentMin*num))
+		maxProduct = max(maxProduct, currentMax)
+	}
+	return maxProduct
 }
