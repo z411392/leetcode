93617ca86f2de64aa20a31da953ad05f94c9eebb
z411392 <z411392@gmail.com>
2025-02-12 14:19:46 +0800

    Wed Feb 12 14:19:46 CST 2025

---
+++ tests/medium/evaluate_reverse_polish_notation/solution_test.go
@@ -0,0 +1,32 @@
+package evaluate_reverse_polish_notation
+
+import (
+	"testing"
+)
+
+func Test_evalRPN_1(t *testing.T) {
+	// t.SkipNow()
+	got := evalRPN([]string{"2", "1", "+", "3", "*"})
+	expected := 9
+	if got != expected {
+		t.Fatalf("expected %v, got %v\n", expected, got)
+	}
+}
+
+func Test_evalRPN_2(t *testing.T) {
+	// t.SkipNow()
+	got := evalRPN([]string{"4", "13", "5", "/", "+"})
+	expected := 6
+	if got != expected {
+		t.Fatalf("expected %v, got %v\n", expected, got)
+	}
+}
+
+func Test_evalRPN_3(t *testing.T) {
+	// t.SkipNow()
+	got := evalRPN([]string{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"})
+	expected := 22
+	if got != expected {
+		t.Fatalf("expected %v, got %v\n", expected, got)
+	}
+}
