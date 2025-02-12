ddf4bc532a6ebcf1c04430002f2b0efb556d31d5
z411392 <z411392@gmail.com>
2025-02-12 17:08:25 +0800

    Wed Feb 12 17:08:25 CST 2025

--- tests/medium/fraction_to_recurring_decimal/solution.go
+++ tests/medium/fraction_to_recurring_decimal/solution.go
@@ -1,5 +1,10 @@
 package fraction_to_recurring_decimal
 
+import (
+	"strconv"
+	"strings"
+)
+
 /*
 Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
 
@@ -10,5 +15,72 @@ If multiple answers are possible, return any of them.
 It is guaranteed that the length of the answer string is less than 104 for all the given inputs.
 */
 func fractionToDecimal(numerator int, denominator int) string {
-	return ""
+	if numerator == 0 {
+		return "0"
+	}
+	negative := false
+	if numerator < 0 {
+		numerator = -numerator
+		negative = !negative
+	}
+	if denominator < 0 {
+		denominator = -denominator
+		negative = !negative
+	}
+	digits := &strings.Builder{}
+	numeratorMap := map[int]int{}
+	decimalIndex := -1
+	repeatStart := -1
+	repeatEnd := -1
+	for {
+		// 如果已經處理過相同的被除數表示遇到循環結束
+		if value, seen := numeratorMap[numerator]; seen {
+			repeatStart = value
+			repeatEnd = len(digits.String())
+			break
+		}
+		// 否則假設被除數為循環開始
+		numeratorMap[numerator] = len(digits.String())
+		// 每次被除數小於除數時補零；第一次補零時保留當時字串的長度（會恰好是小數點的 index）
+		if numerator < denominator {
+			digits.WriteRune('0')
+		} else {
+			remainder := numerator % denominator
+			quotient := numerator / denominator
+			// fmt.Printf("%v/%v=%v\n", numerator, denominator, quotient)
+			// 餘數為零時表示能整除不必考慮無限循環的問題
+			if remainder == 0 {
+				digits.WriteString(strconv.Itoa(quotient))
+				break
+			}
+			numerator = remainder
+			digits.WriteString(strconv.Itoa(quotient))
+		}
+		if decimalIndex == -1 {
+			decimalIndex = len(digits.String())
+		}
+		numerator *= 10
+	}
+	result := digits.String()
+	// fmt.Printf("result=%s, decimalIndex=%v, repeatStart=%v, repeatEnd=%v, negative=%v\n", result, decimalIndex, repeatStart, repeatEnd, negative)
+	// 如果從整數部分就已經開始循環，要將循環的部分往右移
+	if repeatStart != -1 && decimalIndex != -1 && repeatStart < decimalIndex {
+		shift := decimalIndex - repeatStart
+		result += result[repeatStart:decimalIndex]
+		repeatStart += shift
+		repeatEnd += shift
+	}
+	if repeatEnd != -1 {
+		result = result[:repeatEnd] + ")"
+	}
+	if repeatStart != -1 {
+		result = result[:repeatStart] + "(" + result[repeatStart:]
+	}
+	if decimalIndex != -1 {
+		result = result[:decimalIndex] + "." + result[decimalIndex:]
+	}
+	if negative {
+		result = "-" + result
+	}
+	return result
 }
--- tests/medium/fraction_to_recurring_decimal/solution_test.go
+++ tests/medium/fraction_to_recurring_decimal/solution_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func Test_fractionToDecimal_1(t *testing.T) {
-	// t.SkipNow()
+	t.SkipNow()
 	got := fractionToDecimal(1, 2)
 	expected := "0.5"
 	if got != expected {
@@ -14,7 +14,7 @@ func Test_fractionToDecimal_1(t *testing.T) {
 }
 
 func Test_fractionToDecimal_2(t *testing.T) {
-	// t.SkipNow()
+	t.SkipNow()
 	got := fractionToDecimal(2, 1)
 	expected := "2"
 	if got != expected {
@@ -23,9 +23,36 @@ func Test_fractionToDecimal_2(t *testing.T) {
 }
 
 func Test_fractionToDecimal_3(t *testing.T) {
-	// t.SkipNow()
+	t.SkipNow()
 	got := fractionToDecimal(4, 333)
 	expected := "0.(012)"
+	if got != expected {
+		t.Fatalf("expected %v, got %v\n", expected, got)
+	}
+}
+
+func Test_fractionToDecimal_4(t *testing.T) {
+	t.SkipNow()
+	got := fractionToDecimal(22, 7)
+	expected := "3.(142857)"
+	if got != expected {
+		t.Fatalf("expected %v, got %v\n", expected, got)
+	}
+}
+
+func Test_fractionToDecimal_5(t *testing.T) {
+	t.SkipNow()
+	got := fractionToDecimal(-50, 8)
+	expected := "-6.25"
+	if got != expected {
+		t.Fatalf("expected %v, got %v\n", expected, got)
+	}
+}
+
+func Test_fractionToDecimal_6(t *testing.T) {
+	// t.SkipNow()
+	got := fractionToDecimal(420, 226)
+	expected := "1.(8584070796460176991150442477876106194690265486725663716814159292035398230088495575221238938053097345132743362831"
 	if got != expected {
 		t.Fatalf("expected %v, got %v\n", expected, got)
 	}
