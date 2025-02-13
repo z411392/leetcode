7b5804984ce8bef7f091be7ad890518b226c7549
z411392 <z411392@gmail.com>
2025-02-13 17:33:10 +0800

    Thu Feb 13 17:33:10 CST 2025

--- tests/medium/course_schedule/solution.go
+++ tests/medium/course_schedule/solution.go
@@ -7,5 +7,43 @@ For example, the pair [0, 1], indicates that to take course 0 you have to first 
 Return true if you can finish all courses. Otherwise, return false.
 */
 func canFinish(numCourses int, prerequisites [][]int) bool {
-	return false
+	prerequisitesMap := map[int]([]int){}
+	statusMap := map[int]int{}
+	const (
+		unvisited = 0
+		visiting  = 1
+		visited   = 2
+	)
+	for _, pair := range prerequisites {
+		course, prerequisite := pair[0], pair[1]
+		if _, exists := prerequisitesMap[course]; !exists {
+			prerequisitesMap[course] = []int{}
+			statusMap[course] = unvisited
+		}
+		prerequisitesMap[course] = append(prerequisitesMap[course], prerequisite)
+	}
+	var dfs func(course int) bool
+	dfs = func(course int) bool {
+		if statusMap[course] == visiting {
+			return true
+		}
+		if statusMap[course] == visited {
+			return false
+		}
+		statusMap[course] = visiting
+		for _, prerequisite := range prerequisitesMap[course] {
+			if dfs(prerequisite) {
+				return true
+			}
+		}
+		statusMap[course] = visited
+		return false
+	}
+	for course := range numCourses {
+		conflict := dfs(course)
+		if conflict {
+			return false
+		}
+	}
+	return true
 }
