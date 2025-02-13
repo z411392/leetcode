07c0690e0c86ef8926ddb30e8e4fa458de3b1bab
z411392 <z411392@gmail.com>
2025-02-13 15:44:44 +0800

    Thu Feb 13 15:44:44 CST 2025

--- tests/medium/number_of_islands/solution.go
+++ tests/medium/number_of_islands/solution.go
@@ -6,5 +6,38 @@ Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0'
 An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
 */
 func numIslands(grid [][]byte) int {
-	return 0
+	var dfs func(row, column int)
+	dfs = func(row, column int) {
+		if row < 0 {
+			return
+		}
+		if column < 0 {
+			return
+		}
+		if row >= len(grid) {
+			return
+		}
+		if column >= len(grid[row]) {
+			return
+		}
+		if grid[row][column] == '0' {
+			return
+		}
+		grid[row][column] = '0'
+		dfs(row-1, column)
+		dfs(row+1, column)
+		dfs(row, column-1)
+		dfs(row, column+1)
+	}
+	nums := 0
+	for row, cells := range grid {
+		for column, cell := range cells {
+			if cell == '0' {
+				continue
+			}
+			nums += 1
+			dfs(row, column)
+		}
+	}
+	return nums
 }
