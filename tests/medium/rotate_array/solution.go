f81e3a0e823e4d950d889da4cca5c313a484ad89
z411392 <z411392@gmail.com>
2025-02-13 14:27:57 +0800

    Thu Feb 13 14:27:57 CST 2025

--- tests/medium/rotate_array/solution.go
+++ tests/medium/rotate_array/solution.go
@@ -1,13 +1,14 @@
 package rotate_array
 
-import "slices"
-
 /*
 Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
 */
 func rotate(nums []int, k int) {
+	original := make([]int, len(nums))
+	copy(original, nums)
 	shift := k % len(nums)
-	for i, num := range slices.Concat(nums[len(nums)-shift:], nums[:len(nums)-shift]) {
-		nums[i] = num
+	for i, num := range original {
+		j := (i + shift) % len(nums)
+		nums[j] = num
 	}
 }
