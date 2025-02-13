b18ef26bb6e75dbcce7f4ad6c69a6475bfdef1ac
z411392 <z411392@gmail.com>
2025-02-13 14:54:35 +0800

    Thu Feb 13 14:54:35 CST 2025

---
+++ tests/medium/house_robber/solution_test.go
@@ -0,0 +1,23 @@
+package house_robber
+
+import (
+	"testing"
+)
+
+func Test_rob_1(t *testing.T) {
+	// t.SkipNow()
+	got := rob([]int{1, 2, 3, 1})
+	expected := 4
+	if got != expected {
+		t.Fatalf("expected %v, got %v\n", expected, got)
+	}
+}
+
+func Test_rob_2(t *testing.T) {
+	// t.SkipNow()
+	got := rob([]int{2, 7, 9, 3, 1})
+	expected := 12
+	if got != expected {
+		t.Fatalf("expected %v, got %v\n", expected, got)
+	}
+}
