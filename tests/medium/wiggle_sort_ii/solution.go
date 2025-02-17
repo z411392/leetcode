42b9e9f592994b400a3b710d40fb19b154511961
z411392 <z411392@gmail.com>
2025-02-17 15:56:31 +0800

    Mon Feb 17 15:56:31 CST 2025

--- tests/medium/wiggle_sort_ii/solution.go
+++ tests/medium/wiggle_sort_ii/solution.go
@@ -1,5 +1,27 @@
 package wiggle_sort_ii
 
-func wiggleSort(nums []int) {
+import (
+	"slices"
+)
 
+func wiggleSort(nums []int) {
+	cloned := slices.Clone(nums)
+	slices.Sort(cloned)
+	left, right := cloned[:len(nums)/2], cloned[len(nums)/2:]
+	i := -1
+	for {
+		if i == len(nums)-1 {
+			break
+		}
+		if len(left) > 0 {
+			i += 1
+			nums[i] = left[0]
+			left = left[1:]
+		}
+		if len(right) > 0 {
+			i += 1
+			nums[i] = right[0]
+			right = right[1:]
+		}
+	}
 }
--- tests/medium/wiggle_sort_ii/solution_test.go
+++ tests/medium/wiggle_sort_ii/solution_test.go
@@ -9,7 +9,7 @@ func Test_wiggleSort_1(t *testing.T) {
 	// t.SkipNow()
 	got := []int{1, 5, 1, 1, 6, 4}
 	wiggleSort(got)
-	expected := []int{1, 6, 1, 5, 1, 4}
+	expected := []int{1, 4, 1, 5, 1, 6}
 	if !reflect.DeepEqual(got, expected) {
 		t.Fatalf("expected %v, got %v\n", expected, got)
 	}
@@ -19,7 +19,7 @@ func Test_wiggleSort_2(t *testing.T) {
 	// t.SkipNow()
 	got := []int{1, 3, 2, 2, 3, 1}
 	wiggleSort(got)
-	expected := []int{2, 3, 1, 3, 1, 2}
+	expected := []int{1, 2, 1, 3, 2, 3}
 	if !reflect.DeepEqual(got, expected) {
 		t.Fatalf("expected %v, got %v\n", expected, got)
 	}
