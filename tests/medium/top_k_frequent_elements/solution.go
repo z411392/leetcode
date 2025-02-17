afd22e240833c0dc9e8365bdb2ea460adeb8d257
z411392 <z411392@gmail.com>
2025-02-17 18:13:11 +0800

    Mon Feb 17 18:13:11 CST 2025

--- tests/medium/top_k_frequent_elements/solution.go
+++ tests/medium/top_k_frequent_elements/solution.go
@@ -1,52 +1,23 @@
 package top_k_frequent_elements
 
-import "container/heap"
+import (
+	"maps"
+	"slices"
+)
 
 func topKFrequent(nums []int, k int) []int {
 	// https://leetcode.com/problems/top-k-frequent-elements/solutions/519149/go-8ms-using-priorityqueue/
-	seen := make(map[int]int)
-	for _, n := range nums {
-		seen[n]++
+	counters := make(map[int]int)
+	for _, num := range nums {
+		counters[num] += 1
 	}
-
-	q := &priorityQueue{}
-	heap.Init(q)
-	for val, cnt := range seen {
-		heap.Push(q, element{value: val, count: cnt})
-		if q.Len() > k {
-			heap.Pop(q)
+	keys := slices.Collect(maps.Keys(counters))
+	slices.SortFunc(keys, func(a, b int) int {
+		cmp := counters[b] - counters[a]
+		if cmp == 0 {
+			return a - b
 		}
-	}
-
-	ans := make([]int, k, k)
-	for i := 1; i <= k; i++ {
-		v := heap.Pop(q)
-		if s, ok := v.(element); ok {
-			ans[k-i] = s.value
-		}
-	}
-	return ans
-}
-
-type element struct {
-	value int
-	count int
-}
-
-type priorityQueue []element
-
-func (q priorityQueue) Len() int           { return len(q) }
-func (q priorityQueue) Less(i, j int) bool { return q[i].count < q[j].count }
-func (q priorityQueue) Swap(i, j int)      { q[i], q[j] = q[j], q[i] }
-
-func (q *priorityQueue) Push(x interface{}) {
-	*q = append(*q, x.(element))
-}
-
-func (q *priorityQueue) Pop() interface{} {
-	old := *q
-	n := len(old)
-	x := old[n-1]
-	*q = old[0 : n-1]
-	return x
+		return cmp
+	})
+	return keys[:k]
 }
--- tests/medium/top_k_frequent_elements/solution_test.go
+++ tests/medium/top_k_frequent_elements/solution_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func Test_topKFrequent_1(t *testing.T) {
-	// t.SkipNow()
+	t.SkipNow()
 	got := topKFrequent([]int{1, 1, 1, 2, 2, 3}, 2)
 	expected := []int{1, 2}
 	if !reflect.DeepEqual(got, expected) {
@@ -17,9 +17,18 @@ func Test_topKFrequent_1(t *testing.T) {
 }
 
 func Test_topKFrequent_2(t *testing.T) {
-	// t.SkipNow()
+	t.SkipNow()
 	got := topKFrequent([]int{1}, 1)
 	expected := []int{1}
+	if !reflect.DeepEqual(got, expected) {
+		t.Fatalf("expected %v, got %v\n", expected, got)
+	}
+}
+
+func Test_topKFrequent_3(t *testing.T) {
+	// t.SkipNow()
+	got := topKFrequent([]int{3, 2, 3, 1, 2, 4, 5, 5, 6, 7, 7, 8, 2, 3, 1, 1, 1, 10, 11, 5, 6, 2, 4, 7, 8, 5, 6}, 10)
+	expected := []int{1, 2, 5, 3, 6, 7, 4, 8, 10, 11}
 	if !reflect.DeepEqual(got, expected) {
 		t.Fatalf("expected %v, got %v\n", expected, got)
 	}
