47d144b5a44b525e37bedb632307d3b74aa5d97f
z411392 <z411392@gmail.com>
2025-02-18 17:40:35 +0800

    Tue Feb 18 17:40:35 CST 2025

--- tests/medium/longest_substring_with_at_least_k_repeating_characters/solution.go
+++ tests/medium/longest_substring_with_at_least_k_repeating_characters/solution.go
@@ -1,70 +1,52 @@
 package longest_substring_with_at_least_k_repeating_characters
 
 func longestSubstring(s string, k int) int {
-	counters := map[rune]int{}
-	for _, c := range s {
-		counters[c] += 1
-	}
-	removing := map[rune]int{}
-	for c, counter := range counters {
-		if counter >= k {
-			continue
-		}
-		removing[c] = counter
-	}
-	left := 0
-	right := len(s) - 1
-	for {
-		if len(removing) == 0 {
-			break
-		}
-		// fmt.Printf("%s\n", s[left:right+1])
-		if left >= right {
-			break
-		}
-		// time.Sleep(time.Duration(500 * time.Millisecond))
-		found := false
-		fromLeft := rune(s[left])
-		_, exists := removing[fromLeft]
-		if exists {
-			found = true
-			left += 1
-			counters[fromLeft] -= 1
-			if counters[fromLeft] == 0 {
-				delete(removing, fromLeft)
-			}
-		}
-		fromRight := rune(s[right])
-		_, exists = removing[fromRight]
-		if exists {
-			found = true
-			right -= 1
-			counters[fromRight] -= 1
-			if counters[fromRight] == 0 {
-				delete(removing, fromRight)
-			}
-		}
-		if !found {
-			if counters[fromLeft] > counters[fromRight] {
-				counters[fromLeft] -= 1
-				if counters[fromLeft] < k {
-					removing[fromLeft] = counters[fromLeft]
-				}
-				left += 1
-			} else {
-				counters[fromRight] -= 1
-				if counters[fromRight] < k {
-					removing[fromRight] = counters[fromRight]
-				}
-				right -= 1
-			}
-		}
-	}
-	if left == right {
-		if k == 1 {
-			return 1
-		}
+	// 如果字串長度小於 k，不可能有符合條件的子字串
+	if len(s) < k {
 		return 0
 	}
-	return len(s[left : right+1])
+
+	// 計算每個字元出現的頻率
+	count := make(map[byte]int)
+	for i := 0; i < len(s); i++ {
+		count[s[i]]++
+	}
+
+	// 檢查是否所有字元都符合條件
+	allValid := true
+	for i := 0; i < len(s); i++ {
+		if count[s[i]] < k {
+			allValid = false
+			break
+		}
+	}
+
+	// 如果所有字元都符合條件，返回整個字串長度
+	if allValid {
+		return len(s)
+	}
+
+	// 找出不符合條件的字元，以此字元分割字串
+	maxLen := 0
+	start := 0
+	for i := 0; i < len(s); i++ {
+		if count[s[i]] < k {
+			// 遞迴處理子字串
+			subLen := longestSubstring(s[start:i], k)
+			if subLen > maxLen {
+				maxLen = subLen
+			}
+			start = i + 1
+		}
+	}
+
+	// 處理最後一個子字串
+	if start < len(s) {
+		subLen := longestSubstring(s[start:], k)
+		if subLen > maxLen {
+			maxLen = subLen
+		}
+	}
+
+	return maxLen
 }
